5558437d2af88851197c94b23a1eff29b5e2c79f
ninamandl <nina.mandl@campus.lmu.de>
2021-11-29 12:11:32 +0900

    sticky top und nav bar

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
-function on() {
-	document.getElementById("overlay").style.display = "block";
-  }
+function content01_on() {
+	document.getElementById("overlay01").style.display = "block";
+}
   
-  function off() {
-	document.getElementById("overlay").style.display = "none";
-  }
+function content01_off() {
+	document.getElementById("overlay01").style.display = "none";
+}
+
+
+function content02_on() {
+	document.getElementById("overlay02").style.display = "block";
+}
+  
+function content02_off() {
+	document.getElementById("overlay02").style.display = "none";
+}
