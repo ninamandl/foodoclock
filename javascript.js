abe493b41d455cf8baeaf223779acf7a1f0785c3
ninamandl <nina.mandl@campus.lmu.de>
2021-12-20 02:12:13 +0900

    profile

--- javascript.js
+++ javascript.js
@@ -15,3 +15,20 @@ function content02_off() {
 	document.getElementById("overlay02").style.display = "none";
 }
 
+
+var count = 1;
+    function change(btn, color) {
+        var property = document.getElementById(btn);
+        if (count == 0) {
+            property.style.backgroundColor = "#FFFFFF"
+			property.style.color = "#707070"
+			property.value = "follow"
+            count = 1;        
+        }
+        else {
+            property.style.backgroundColor = "#5e7d5e"
+			property.style.color = "#FFFFFF"
+			property.value = "unfollow"
+            count = 0;
+        }
+	}
